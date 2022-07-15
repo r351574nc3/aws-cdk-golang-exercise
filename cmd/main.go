7f493c95824bae9e9ff010ed8185784390cdc89d
Leo (중광) Przybylski <r351574nc3@gmail.com>
2022-07-15 08:13:37 -0700

    Initial checkin and project stubbing

---
+++ cmd/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/constructs-go/constructs/v10"
+	// "github.com/aws/jsii-runtime-go"
+)
+
+type AppStackProps struct {
+	awscdk.StackProps
+}
+
+func NewAppStack(scope constructs.Construct, id string, props *AppStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	// The code that defines your stack goes here
+
+	// example resource
+	// queue := awssqs.NewQueue(stack, jsii.String("AppQueue"), &awssqs.QueueProps{
+	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
+	// })
+
+	return stack
+}
+
+func main() {
+	app := awscdk.NewApp(nil)
+
+	NewAppStack(app, "AppStack", &AppStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+// env determines the AWS environment (account+region) in which our stack is to
+// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+func env() *awscdk.Environment {
+	return &awscdk.Environment{
+		Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+		Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	}
+}
